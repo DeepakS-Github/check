4396f63d858b1d7c3d203586a746754809255b8e
DeepakS-Github <deepakanonymous75@gmail.com>
2023-05-13 00:51:22 +0530

    D

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+
+// Get Item Data By Category
+app.get("/api", async (req, res) => {
+  res.send("API running");
+});
+
+
+app.listen(3700);
